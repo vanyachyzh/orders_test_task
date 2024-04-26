99b272203d5bd6a344708a876a030b72f607d207
Ivan <ivanchyzh97@gmail.com>
2024-04-26 16:58:56 +0300

    Implement Orders page

--- src/pages/Orders/index.tsx
+++ src/pages/Orders/index.tsx
@@ -1,7 +1,37 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import Button from '../../components/Button';
+import { COOKIE_TOKEN_NAME } from '../../constants';
+import { auth } from '../../firebase';
+import { deleteCookie } from '../../utils/cookies';
+import { handleError } from '../../utils/handleError';
+import { toast } from 'react-toastify';
+
 const Orders = () => {
-  return <section className="text-[2rem] font-semibold">This is the order page
-  
-  </section>;
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const signOut = async () => {
+    setIsLoading(true);
+    try {
+      await auth.signOut();
+      navigate('/login');
+      deleteCookie(COOKIE_TOKEN_NAME);
+      toast.success("You logged out!")
+    } catch (error) {
+      handleError(error, 'Error logging out');
+    }
+    setIsLoading(false);
+  };
+  return (
+    <section className="flex flex-col items-center gap-[2rem] text-[2rem] font-semibold">
+      This is the order page
+      <Button onClick={signOut} isLoading={isLoading}>
+        Log Out
+      </Button>
+    </section>
+  );
 };
 
 export default Orders;
