ff808319052f52fb872cd776b2dd1953c5906693
Ivan <ivanchyzh97@gmail.com>
2024-04-27 16:58:20 +0300

    Highlight current path

--- src/layouts/Sidebar/index.tsx
+++ src/layouts/Sidebar/index.tsx
@@ -1,15 +1,21 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import { twMerge } from 'tailwind-merge';
 
 import { SIDEBAR_LINKS } from './constants';
 
 const Sidebar = () => {
+  const { pathname } = useLocation();
+
   return (
-    <ul className="mt-6 space-y-1 p-[1rem] bg-white/20 rounded-[1.2rem] h-min">
+    <ul className="mt-6 h-min space-y-1 rounded-[1.2rem] bg-white/20 p-[1rem]">
       {SIDEBAR_LINKS.map(link => (
         <li key={link.name}>
           <Link
             to={link.to}
-            className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+            className={twMerge(
+              'block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700',
+              pathname === link.to && 'bg-gray-100',
+            )}
           >
             {link.name}
           </Link>
